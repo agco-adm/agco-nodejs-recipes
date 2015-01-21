f40d7febb8e56f2f3c24eaa724a2fead7ace965c
kristof sajdak <kristof.sajdak@gmail.com>
2015-01-21 13:30:34 +0100

    added example code of a before transform code block throwing a JSONAPI_Error

--- app/api.js
+++ app/api.js
@@ -11,11 +11,36 @@ var options = {
 // analogue to the examples used on jsonapi.org
 var harvestApp = harvest(options)
     .resource('post', {
-        title: String
+        title: String,
+        closed: Boolean
     })
     .resource('comment', {
         body: String,
         post: 'post'
+    })
+    .before(function (req, res) {
+        // alias 'this' for easy usage within the promise 'then' function callback scope
+        var that = this;
+        // if modified, fetch the associated post for the comment
+        return harvestApp.adapter.findMany('post', {id: this.links.post})
+            .then(function (posts) {
+                if (posts.length > 0) {
+                    // if the post is closed, throw a JSONAPI_Error with a 400 status code and some detail message
+                    // harvest will take care of converting this to a jsonapi spec errors representation
+                    // http://jsonapi.org/format/#errors
+                    var closed = !!posts[0].closed;
+                    if (closed) {
+                        throw new harvest.JSONAPI_Error({
+                            status: 400,
+                            detail: 'post is closed, comments are disabled'
+                        });
+                    }
+                }
+                // important ! return that (this alias)
+                // to resume normal processing flow
+                return that;
+
+            })
     });
 
 module.exports = harvestApp;
